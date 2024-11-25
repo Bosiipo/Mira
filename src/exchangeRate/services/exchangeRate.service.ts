f78d3925cd6e855217cfabaead7b26e6035a4906
bosiipo <olabosipo07@gmail>
2024-11-25 05:45:00 +0100

    Fix: clean up exchange rate service

--- src/exchangeRate/services/exchangeRate.service.ts
+++ src/exchangeRate/services/exchangeRate.service.ts
@@ -3,61 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { ExchangeRate } from '../models/exchangeRate.entity';
 import { Currency, TransactionKey } from '../dto/exchangeRate.enum';
-import { RateEntry, ResponseObject } from '../responses/exchangeRate.response';
-
-const validateCountryCode = (input: string): boolean => {
-  // Regular expression to check for exactly three capital letters
-  const regex = /^[A-Z]{3}$/;
-
-  // Test the input against the regex
-  return regex.test(input);
-}
-
-const validateCurrency = (rawCurrencyPair) => {
-  // console.log({ rawCurrencyPair });
-  const currencyEnum = Object.values(Currency);
-  // console.log({ currencyEnum });
-  const splitCurrency = rawCurrencyPair.split(' ');
-  // console.log({ splitCurrency });
-  if (splitCurrency.length < 2) {
-    throw new Error('Missing single space after currency pair!');
-  }
-  const currencyPair = splitCurrency[0].replace(/n/g, '');
-  const [SourceCurrency, DestinationCurrency] = currencyPair.split('-');
-  // console.log({ SourceCurrency, DestinationCurrency, currencyPair });
-  if (!validateCountryCode(SourceCurrency)) {
-    throw new Error(
-      `${SourceCurrency} does not follow rules and constraints for currency input!`,
-    );
-  }
-
-  if (!validateCountryCode(DestinationCurrency)) {
-    throw new Error(
-      `${DestinationCurrency} does not follow rules and constraints for currency input!`,
-    );
-  }
-  return [SourceCurrency, DestinationCurrency];
-};
-
-const validateRate = ({ transactionKey, transactionValue }) => {
-  // console.log({ transactionKey, transactionValue });
-  const currencyTransactionEnum = Object.values(TransactionKey);
-  if (!currencyTransactionEnum.includes(transactionKey)) {
-    throw new Error('Invalid transaction key!');
-  }
-
-  if (transactionKey === TransactionKey.CAP) {
-    if (transactionValue.includes('.')) {
-      throw new Error('No decimals are allowed for CAP!');
-    }
-  }
-  const parsedDigit = Number(transactionValue);
-  if (Number.isNaN(parsedDigit) || parsedDigit < 0) {
-    throw new Error('Value is not valid!');
-  }
-
-  return parsedDigit;
-};
+import { ResponseObject } from '../responses/exchangeRate.response';
 
 @Injectable()
 export class exchangeRateService {
@@ -65,6 +11,58 @@ export class exchangeRateService {
     @InjectRepository(ExchangeRate)
     private exchangeRateRepository: Repository<ExchangeRate>,
   ) {}
+
+  validateCountryCode = (input: string): boolean => {
+    // Regular expression to check for exactly three capital letters
+    const regex = /^[A-Z]{3}$/;
+  
+    // Test the input against the regex
+    return regex.test(input);
+  }
+  
+  validateCurrency = (rawCurrencyPair) => {
+    // console.log({ rawCurrencyPair });
+    const splitCurrency = rawCurrencyPair.split(' ');
+    // console.log({ splitCurrency });
+    if (splitCurrency.length < 2) {
+      throw new Error('Missing single space after currency pair!');
+    }
+    const currencyPair = splitCurrency[0].replace(/n/g, '');
+    const [SourceCurrency, DestinationCurrency] = currencyPair.split('-');
+    // console.log({ SourceCurrency, DestinationCurrency, currencyPair });
+    if (!this.validateCountryCode(SourceCurrency)) {
+      throw new Error(
+        `${SourceCurrency} does not follow rules and constraints for currency input!`,
+      );
+    }
+  
+    if (!this.validateCountryCode(DestinationCurrency)) {
+      throw new Error(
+        `${DestinationCurrency} does not follow rules and constraints for currency input!`,
+      );
+    }
+    return [SourceCurrency, DestinationCurrency];
+  };
+  
+  validateRate = ({ transactionKey, transactionValue }) => {
+    // console.log({ transactionKey, transactionValue });
+    const currencyTransactionEnum = Object.values(TransactionKey);
+    if (!currencyTransactionEnum.includes(transactionKey)) {
+      throw new Error('Invalid transaction key!');
+    }
+  
+    if (transactionKey === TransactionKey.CAP) {
+      if (transactionValue.includes('.')) {
+        throw new Error('No decimals are allowed for CAP!');
+      }
+    }
+    const parsedDigit = Number(transactionValue);
+    if (Number.isNaN(parsedDigit) || parsedDigit < 0) {
+      throw new Error('Value is not valid!');
+    }
+  
+    return parsedDigit;
+  };
 
   async saveExchangeRates(data: ExchangeRate[]): Promise<ExchangeRate[]> {
     // Save the data to the database
@@ -78,49 +76,58 @@ export class exchangeRateService {
     if (statement.length === 0) {
       throw new Error('Enter a statement!');
     }
+
     const splitInput: string[] = statement.split('\\');
+    const refinedData: ExchangeRate[] = [];
+    const prototype: ExchangeRate = {
+      SourceCurrency: Currency.null,
+      DestinationCurrency: Currency.null,
+      BuyPrice: 0,
+      SellPrice: 0,
+      CapAmount: 0,
+    };
+    let startingPointer: number = 0;
+    let closingPointer: number = 0;
+    let closingBlock: string = splitInput[closingPointer];
     const firstIteration: string = splitInput[0];
     if (firstIteration.split(' ').length !== 2) {
       throw new Error('Input not complete!');
     }
-    let startingPointer: number = 0;
-    let closingPointer: number = 0;
-    let closingBlock: string = splitInput[closingPointer];
-    const refinedData: ExchangeRate[] = [];
 
     while (startingPointer <= splitInput.length) {
       const current: string = splitInput[closingPointer];
       startingPointer = closingPointer;
       closingPointer += 4;
       closingBlock = splitInput[closingPointer];
+
       if (closingBlock === undefined) break;
+
       if (!closingBlock.includes('}')) {
         throw new Error('Invalid syntax!');
       }
-      const prototype: ExchangeRate = {
-        SourceCurrency: Currency.null,
-        DestinationCurrency: Currency.null,
-        BuyPrice: 0,
-        SellPrice: 0,
-        CapAmount: 0,
-      }
+
       while (current.includes('{')) {
         const currentIteration: string = splitInput[startingPointer];
+
         if (currentIteration === 'n}') {
           break;
         }
+
         const [SourceCurrency, DestinationCurrency] =
-          validateCurrency(currentIteration);
+          this.validateCurrency(currentIteration);
+  
         prototype.SourceCurrency = SourceCurrency;
         prototype.DestinationCurrency = DestinationCurrency;
         startingPointer++;
+
         while (startingPointer < closingPointer) {
           const rate: string = splitInput[startingPointer];
           const [, , transactionKey, transactionValue] = rate.trim().split(' ');
-          const refinedRate: number = validateRate({
+          const refinedRate: number = this.validateRate({
             transactionKey,
             transactionValue,
           });
+
           if (transactionKey === TransactionKey.BUY) {
             prototype.BuyPrice = refinedRate;
           } else if (transactionKey === TransactionKey.SELL) {
@@ -128,6 +135,7 @@ export class exchangeRateService {
           } else if (transactionKey === TransactionKey.CAP){
             prototype.CapAmount = refinedRate;
           }
+
           startingPointer++;
         }
       }
